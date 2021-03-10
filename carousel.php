7534b9987dcd60da5747ceed7f04824021b34603
Zteveluke <steveluke2606@gmail.com>
2021-03-10 15:19:35 +0530

    php

--- carousel.php
+++ carousel.php
@@ -17,20 +17,20 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="css.html">Home</a>
+                <a class="nav-link active" aria-current="page" href="css.php">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="book.html">Link</a>
+                <a class="nav-link" href="book.php">Link</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Dropdown
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="index.html">Action</a></li>
-                  <li><a class="dropdown-item" href="container3.html">Another action</a></li>
+                  <li><a class="dropdown-item" href="index.php">Action</a></li>
+                  <li><a class="dropdown-item" href="container3.php">Another action</a></li>
                   <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="accordian.html">Something else here</a></li>
+                  <li><a class="dropdown-item" href="accordian.php">Something else here</a></li>
                 </ul>
               </li>
               <li class="nav-item">
